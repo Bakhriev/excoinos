6254f95af1971b99af436eb53259af00e542aaea
Murat <muratbakhriev@gmail.com>
2023-11-12 14:22:30 +0500

    main page hero end

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,6 +3,13 @@ import {burgerMenu} from './functions/burger'
 burgerMenu()
 
 const langDrop = document.querySelector('.lang-drop')
-langDrop.addEventListener('click', () => {
-	langDrop.classList.toggle('active')
+langDrop.addEventListener('click', () => langDrop.classList.toggle('active'))
+
+const converters = document.querySelectorAll('.converter')
+
+converters.forEach(converter => {
+	const convertersSelect = converter.querySelector('.converter-select')
+	convertersSelect.addEventListener('click', () =>
+		converter.classList.toggle('active')
+	)
 })
