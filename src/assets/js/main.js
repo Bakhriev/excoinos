fd29d7d37bb7849a7d34cafb08b895f86e2ca7b2
Murat <muratbakhriev@gmail.com>
2023-11-15 10:30:14 +0500

    progress, file upload

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,23 +1,42 @@
-import {burgerMenu} from './functions/burger'
+// import {burgerMenu} from './functions/burger'
 
-burgerMenu()
+// burgerMenu()
 
-const langDrop = document.querySelector('.lang-drop')
-langDrop.addEventListener('click', () => langDrop.classList.toggle('active'))
+// const langDrop = document.querySelector('.lang-drop')
+// langDrop.addEventListener('click', () => langDrop.classList.toggle('active'))
 
-const converters = document.querySelectorAll('.converter')
+// const converters = document.querySelectorAll('.converter')
 
-converters.forEach(converter => {
-	const convertersSelect = converter.querySelector('.converter-select')
-	convertersSelect.addEventListener('click', () =>
-		converter.classList.toggle('active')
-	)
+// converters.forEach(converter => {
+// 	const convertersSelect = converter.querySelector('.converter-select')
+// 	convertersSelect.addEventListener('click', () =>
+// 		converter.classList.toggle('active')
+// 	)
+// })
+
+// const accordions = document.querySelectorAll('.accordion')
+
+// accordions.forEach(accordion => {
+// 	accordion.addEventListener('click', () =>
+// 		accordion.classList.toggle('active')
+// 	)
+// })
+
+const input = document.querySelector('#file-upload')
+const selected = document.querySelector('.file-selected__selected')
+const selectedClear = document.querySelector('.file-selected__clear')
+input.addEventListener('input', e => {
+	const value = input.value.split('\\').at(-1)
+	if (value.length) {
+		selectedClear.classList.add('active')
+		selected.textContent = value
+	} else {
+		selectedClear.classList.remove('active')
+		selected.textContent = value
+	}
 })
-
-const accordions = document.querySelectorAll('.accordion')
-
-accordions.forEach(accordion => {
-	accordion.addEventListener('click', () =>
-		accordion.classList.toggle('active')
-	)
+selectedClear.addEventListener('click', () => {
+	input.value = ''
+	selectedClear.classList.remove('active')
+	selected.textContent = ''
 })
